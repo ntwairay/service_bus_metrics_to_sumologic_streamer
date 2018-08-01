1df19aec9d9a54f594e383f1e7422ca5b2ffea81
Ray Ng <ray.ng@vibrato.com.au>
2018-08-02 09:56:56 +1000

    merge sumologger.js into index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,52 @@
 const Insights = require('node-insights');
-const SumoLogger = require('./sumologger')
+const SumoLogger = require('sumo-logger');
 
-const newrelicOpts = {
-  insights : new Insights({
-    queryKey: process.env.QUERY_KEY,
-    accountId: process.env.ACCOUNT_ID
-  }),
-  query : { select : 'average(activeMessages)', from: 'AzureServiceBusQueueSample',
-            where  : { providerAccountId: process.env.PROVIDER_ACCOUNT_ID }}
-}
+module.exports = function(context,myTimer) {
 
-const sumoOpts = {
-    endpoint: process.env.ENDPOINT,
-    sessionKey: process.env.SESSIONKEY,
-    sendErrors: true,
-    sourceName: process.env.SOURCENAME,
-    sourceCategory: process.env.SOURCECATEGORY,
-    onSuccess: () => {
-      console.log("streaming log to sumologic at " + new Date())
-    },
-    onError: (err) => {
-      console.log(err)
-    }
-    // ... any other options ...
-};
+  const newrelicOpts = {
+    insights : new Insights({
+      queryKey: process.env.QUERY_KEY,
+      accountId: process.env.ACCOUNT_ID
+    }),
+    query : { select : 'average(activeMessages)', from: 'AzureServiceBusQueueSample',
+              where  : { providerAccountId: process.env.PROVIDER_ACCOUNT_ID }}
+  }
 
-let metricsProcessor = (newrelic, sumo) => {
-  newrelic.insights.query(newrelic.query, (err, responseBody) => {
+  const sumoOpts = {
+      endpoint: process.env.ENDPOINT,
+      sessionKey: process.env.SESSIONKEY,
+      sendErrors: true,
+      sourceName: process.env.SOURCENAME,
+      sourceCategory: process.env.SOURCECATEGORY,
+      onSuccess: () => {
+        context.log("streaming log to sumologic at " + new Date())
+      },
+      onError: (err) => {
+        context.log(err)
+      }
+      // ... any other options ...
+  };
+
+  let metricsProcessor = (newrelic, sumo) => {
+    newrelic.insights.query(newrelic.query, (err, responseBody) => {
+      if (err) {
+        context.log(err)
+      }
+      else {
+        context.log(responseBody.results[0])
+        streamMetricsToSumo(sumo,responseBody.results[0])
+      }
+    })
+  }
+
+  const streamMetricsToSumo = (opts, metrics, err) => {
     if (err) {
-      console.log(err)
+      context.log('Error on Rx: ', err);
+    } else {
+      const sumoLogger = new SumoLogger(opts);
+      sumoLogger.log(metrics)
     }
-    else {
-      console.log(responseBody.results[0])
-      SumoLogger.streamMetricsToSumo(sumo,responseBody.results[0])
-    }
-  })
+  }
+
+  metricsProcessor(newrelicOpts,sumoOpts);
 }
-
-setInterval(metricsProcessor.bind(null,newrelicOpts,sumoOpts), 180000);
-
-
-//7542
-//7804
